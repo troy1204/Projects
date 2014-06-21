6fbf396f2765c2649301d285729bab39ffcd2e1d
troy1204 <divya.iyer1204@gmail.com>
2014-06-21 11:06:49 -0400

    Simple polymorphism/inheritance example in Java

---
+++ java_poly_example/inheritancePolymorphism/TrumpetPlayer.java
@@ -0,0 +1,10 @@
+package inheritancePolymorphism;
+public class TrumpetPlayer extends Musician {
+	public TrumpetPlayer(String n){
+		super(n);
+	}
+	public void practise(){
+   		System.out.println("practising to blow the trumpet");
+    }
+	
+}
